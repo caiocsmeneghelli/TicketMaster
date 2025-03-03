bec709fb8ef28c9d79ee5be5dd806473807307ff
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-03-03 17:15:16 -0300

    feat: inicio do projeto.

--- TicketMaster.Domain/Entities/Movie.cs
+++ TicketMaster.Domain/Entities/Movie.cs
@@ -9,8 +9,11 @@ namespace TicketMaster.Domain.Entities
     public class Movie
     {
         public int Id { get; private set; }
-        public string Title { get; private set; }
-        public string Director { get; private set; }
+        public string? Title { get; private set; }
+        public string? Director { get; private set; }
         public DateTime ReleaseDate { get; private set; }
+        public bool Active { get; private set; }
+        public string? Description { get; private set; }
+        public string? Genre { get; private set; }
     }
 }
--- TicketMaster.Domain/Entities/User.cs
+++ TicketMaster.Domain/Entities/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TicketMaster.Domain.ValueObjects;
+﻿using TicketMaster.Domain.ValueObjects;
 
 namespace TicketMaster.Domain.Entities
 {
