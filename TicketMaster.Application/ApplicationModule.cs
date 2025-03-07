b67a895772cc53db36269fae7be45b609c63bd98
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-03-06 21:46:23 -0300

    feat: add applicationModule e injecao de dependencia.

---
+++ TicketMaster.Application/ApplicationModule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.Application.Queies.Movies.GetAll;
+
+namespace TicketMaster.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatr();
+            return services;
+        }
+
+        private static IServiceCollection AddMediatr(this IServiceCollection service)
+        {
+            service.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllMoviesQuery).Assembly));
+            return service;
+        }
+    }
+}
