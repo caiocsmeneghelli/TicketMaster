84580e10969555c0bb42b0c5c5e2e0cc9333e791
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-03-08 21:45:57 -0300

    feat: add auditoriumController e viewModel

---
+++ TicketMaster.API/Controllers/AuditoriumController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketMaster.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditoriumController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("by-theater/{idTheater}")]
+        public async Task<IActionResult> GetAllByTheater(int idTheater)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ TicketMaster.Application/ViewModels/MovieSessions/MovieSessionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMaster.Application.ViewModels.MovieSessions
+{
+    public class MovieSessionViewModel
+    {
+        public int Id { get; set; }
+        public int IdMovieTheater { get; set; }
+        public int IdMovie { get; set; }
+        public string? Movie { get; set; }
+
+        public int IdTheater { get; set; }
+        public string? Theater { get; private set; }
+        public int IdAuditorium { get; set; }
+        public DateTime SessionTime { get; set; }
+        public int TotalSeats { get; set; }
+        public int ReservedSeats { get; set; }
+        public bool Available { get; set; }
+    }
+}
