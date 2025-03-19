6ee809671962ebdc94e4705ea3af7e97735f5b7b
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-03-18 21:40:22 -0300

    feat: add dbContext, projeto de infra e inicio de configuracao.

---
+++ TicketMaster.Infrastructure/Persistence/Configurations/MovieConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.Domain.Entities;
+
+namespace TicketMaster.Infrastructure.Persistence.Configurations
+{
+    public class MovieConfiguration : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TicketMaster.Infrastructure/Persistence/TicketMasterDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.Domain.Entities;
+
+namespace TicketMaster.Infrastructure.Persistence
+{
+    public class TicketMasterDbContext : DbContext
+    {
+        public TicketMasterDbContext(DbContextOptions<TicketMasterDbContext> options) : base(options)
+        { }
+
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieSession> MovieSessions { get; set; }
+        public DbSet<Theater> Theaters { get; set; }
+        public DbSet<Auditorium> Auditoriums { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
