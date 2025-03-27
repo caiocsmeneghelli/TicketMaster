92f43fd17e6a07ecc19377874357111dc77fa89a
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-03-27 11:52:09 -0300

    feat: add acesso ao banco.

---
+++ TicketMaster.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.Infrastructure.Persistence;
+
+namespace TicketMaster.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddPersistence(configuration);
+            return services;
+        }
+
+        private static IServiceCollection AddPersistence(this IServiceCollection service, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("TicketMasterCs");
+            var serverVersion = ServerVersion.AutoDetect(connectionString);
+            service.AddDbContext<TicketMasterDbContext>(cfg => cfg.UseMySql(connectionString, serverVersion));
+            
+            return service;
+        }
+            
+    }
+}
---
+++ TicketMaster.Infrastructure/Persistence/Repositories/MovieRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.Domain.Entities;
+using TicketMaster.Domain.Repositories;
+
+namespace TicketMaster.Infrastructure.Persistence.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly TicketMasterDbContext _context;
+
+        public MovieRepository(TicketMasterDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Movie>> GetAllActiveAsync(string? query)
+        {
+            return await _context.Movies
+                .Where(m => m.MovieSessions.Any(ms => ms.Available))
+                .ToListAsync();
+        }
+
+        public async Task<List<Movie>> GetAllAsync()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+    }
+}
