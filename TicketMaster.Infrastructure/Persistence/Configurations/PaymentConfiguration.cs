f94b6939b5b00bb8461cd5cc4a4c6d75088e4370
Caio Cesar <caiocsmeneghelli@outlook.com>
2025-04-29 05:53:00 -0300

    feat: removido migrations e add payment e ticket configuration.

---
+++ TicketMaster.Infrastructure/Persistence/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMaster.Domain.Entities;
+
+namespace TicketMaster.Infrastructure.Persistence.Configurations
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.HasKey(p => p.Guid);
+        }
+    }
+}
---
+++ TicketMaster.Infrastructure/Persistence/Configurations/TicketConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TicketMaster.Domain.Entities;
+
+namespace TicketMaster.Infrastructure.Persistence.Configurations
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.HasKey(t => t.Guid);
+
+            builder.HasOne(t => t.Payment)
+                .WithOne()
+                .HasForeignKey<Ticket>(t => t.GuidPayment);
+
+            builder.HasOne(t => t.MovieSession)
+                .WithOne()
+                .HasForeignKey<Ticket>(t => t.GuidMovieSession);
+        }
+    }
+}
